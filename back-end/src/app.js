95e099bd3c529b524e28db8ca0d3361a0940dc2c
ByteZ193 <edwinluisdiaz@gmail.com>
2022-01-05 01:23:14 -0400

    Creating the service to get data from de api employees

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import Router from "./routes/employees.routes";
+import cors from "cors";
 
 const app = express();
 
 app.set("port", process.env.PORT || 4000);
 
+app.use(cors({origin:"http://localhost:4200"}));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ front-end/src/app/components/employee/employee.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.employeeService.getEemployees().subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
+  }
+}
